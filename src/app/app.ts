4d99ee8831f7c33cc63632a94f9e997a968349d1
moussacode <samouh269@gmail.com>
2026-06-23 16:09:33 +0000

    fix :risque

--- src/app/app.ts
+++ src/app/app.ts
@@ -3,6 +3,8 @@ import { CarteSenegal } from "./composants/carte-senegal/carte-senegal";
 import { PanneauMeteo } from "./composants/panneau-meteo/panneau-meteo";
 import { GraphiqueClimatiqueComponent } from './composants/graphique-climatique/graphique-climatique';
 import { IndicateurRisqueComponent } from './composants/indicateur-risque/indicateur-risque';
+import { effect, inject } from '@angular/core';
+import { MeteoStateService } from './services/service-meteo';
 
 import { MeteoService } from './services/meteo-services';
 import { GeolocalisationService } from './services/geolocalisation-services';
@@ -25,30 +27,50 @@ export class App implements OnInit {
 
   historique: JourMeteo[] = [];
   indice!: IndiceRisque;
-
+  private meteoState = inject(MeteoStateService);
   constructor(
-    private meteo: MeteoService,
-    private geo: GeolocalisationService,
-    private risque: RisqueClimatiqueService,
-    private historiqueService: HistoriqueMeteoService
-  ) {}
+  private meteo: MeteoService,
+  private geo: GeolocalisationService,
+  private risque: RisqueClimatiqueService,
+  private historiqueService: HistoriqueMeteoService
+) {
+
+  effect(() => {
+
+    const region = this.meteoState.selectedRegion();
+
+    if (!region) return;
+
+    this.chargerDonnees(region);
+
+  });
+
+}
+chargerDonnees(ville: string) {
+
+  this.meteo.obtenirMeteo(ville).subscribe(donnees => {
+
+    this.historique = this.historiqueService.genererHistorique(
+      donnees.temperature,
+      donnees.humidite
+    );
+
+    this.indice = this.risque.calculerIndiceRisque(
+      donnees.temperature,
+      donnees.humidite
+    );
+
+  });
+
+}
 
   // Un seul ngOnInit, un seul appel API
   async ngOnInit() {
+  try {
     const position = await this.geo.obtenirPosition();
-
-    this.meteo.obtenirMeteo(position.ville).subscribe(donnees => {
-      // Tout est regroupé ici dans un seul subscribe
-      this.historique = this.historiqueService.genererHistorique(
-        donnees.temperature,
-        donnees.humidite
-      );
-      this.indice = this.risque.calculerIndiceRisque(
-        donnees.temperature,
-        donnees.humidite
-      );
-
-      console.log('Risque:', this.indice);
-    });
+    this.chargerDonnees(position.ville);
+  } catch (error) {
+    console.error('Erreur de géolocalisation', error);
   }
+}
 }
